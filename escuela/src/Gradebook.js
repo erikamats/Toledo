867307d68dee8b081b5f939d1394f43db6ea1d29
erikamats <erikamats@hotmail.com>
2018-11-10 11:47:36 -0600

    Friday night gradebook

---
+++ escuela/src/Gradebook.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { studentList } from "./SampleStudent";
+
+class Gradebook extends Component {
+  constructor() {
+    super();
+    this.state = {
+      on: false,
+      studentList
+    };
+    console.log(studentList);
+  }
+
+  render() {
+    return (
+      <div>
+        This will be the Gradebook.
+        <h1> Fernando's Class</h1>
+        <hr />
+       
+          {this.state.studentList.map(kids => (
+            <ul>
+            <li> {kids.name}</li>
+            <li> {kids.hmwk}</li>
+            </ul>
+          ))}
+       
+      </div>
+    );
+  }
+}
+
+export default Gradebook;
---
+++ escuela/src/SampleStudent.js
@@ -0,0 +1,15 @@
+export const studentList = [
+  {
+    name: "Billy",
+    hmwk: 98
+  },
+  {
+    name: "Tom",
+    hmwk: 90
+
+  },
+  {
+    name: "John",
+    hmwk: 75
+  }
+];
