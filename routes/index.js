cf826bfbab158b88088b2c01d896a389db355487
John <entity.john@gmail.com>
2018-11-23 16:14:28 -0600

    add some routes and gradebookController methods for them
    Just to kick things off, some simple routes building on what we already had made

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,12 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // If we were actually using async/await in the controller method we refer to, then we would wrap
 // the method in `catchErrors`
 router.get('/students', gradebookController.getStudents);
-router.get('/students-asynchronously', catchErrors(gradebookController.getStudents));
-
+router.get('/students-asynchronously', catchErrors(gradebookController.getStudentsAsynchronously));
 router.post('/register-student', gradebookController.addStudent);
+
+router.get('/courses', gradebookController.getCourses);
+router.get('/courses-asynchronously', catchErrors(gradebookController.getCoursesAsynchronously));
+router.post('/register-course', gradebookController.addCourse);
 
 
 module.exports = router;
