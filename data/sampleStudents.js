4ee2bfa1a93b14f6adb03f15bd7871eb04b7dfeb
John <entity.john@gmail.com>
2018-11-23 16:33:46 -0600

    created sampleStudents and sampleCourses
    3 12th grader, 3 11th graders. 3 classes on MWF, 3 classes on T-Th.

---
+++ data/sampleStudents.js
@@ -0,0 +1,57 @@
+exports.sampleStudents = [
+  {
+    _id: '58c039938060197ca0b52d4d',
+    firstName: 'Erika',
+    lastName: 'Matsumoto',
+    gender: 'female',
+    dateEnrolled: new Date('01/03/2018'),
+    currentGradeLevel: 11,
+  },
+  {
+    _id: '58c039ee8060197ca0b52d4e',
+    firstName: 'Fernando',
+    lastName: 'Garcia',
+    gender: 'male',
+    dateEnrolled: new Date('06/07/2016'),
+    currentGradeLevel: 11,
+  },
+  {
+    _id: '58c03a428060197ca0b52d4f',
+    firstName: 'John',
+    lastName: 'Moen',
+    gender: 'male',
+    dateEnrolled: new Date('12/10/2018'),
+    currentGradeLevel: 11,
+  },
+  {
+    _id: '58c03a958060197ca0b52d50',
+    firstName: 'Sean',
+    lastName: 'Hagen-Daas',
+    gender: 'male',
+    dateEnrolled: new Date('06/10/2018'),
+    currentGradeLevel: 12,
+  },
+  {
+    _id: '58c03a958060197ca0b52d50',
+    firstName: 'Sue',
+    lastName: 'Danglebean',
+    gender: 'female',
+    dateEnrolled: new Date('06/09/2018'),
+    currentGradeLevel: 12,
+  },
+  {
+    _id: '58c03a958060197ca0b52d50',
+    firstName: 'Arrow',
+    lastName: 'Root',
+    gender: 'female',
+    dateEnrolled: new Date('06/07/2018'),
+    currentGradeLevel: 12,
+  },
+];
+
+// unused ID's
+// 58c05aef8060197ca0b52d55
+// 58c05b558060197ca0b52d56
+// 58c05bcd8060197ca0b52d57
+// 58c05c208060197ca0b52d58
+// 58c05f928060197ca0b52d59
