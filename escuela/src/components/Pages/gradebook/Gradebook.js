52a7a886f8fc3287b9c8fe00f65b71f09b831ba3
John <entity.john@gmail.com>
2018-11-24 17:56:27 -0600

    intializing gradebook section
    Starter container, its test, and the same sample data as the server `data` directory

---
+++ escuela/src/components/Pages/gradebook/Gradebook.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class componentName extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      courses: {}
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Gradebook
+      </div>
+    )
+  }
+}
