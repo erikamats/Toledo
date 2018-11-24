6a1906ae8530a798317b56470db2de11c77c465a
John <entity.john@gmail.com>
2018-11-23 21:18:21 -0600

    add forgotten import

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+
 const gradebookController = require('../controllers/gradebookController');
+const homepageController = require('../controllers/homepageController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 const router = express.Router();
-const { catchErrors } = require('../handlers/errorHandlers');
 
 // So if we go to `localhost:5000/` then the following route
 // is what will happen:
