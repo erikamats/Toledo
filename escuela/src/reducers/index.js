04dd57dd424fe0777b3bfd1c40220c70b93e291e
John <entity.john@gmail.com>
2018-11-23 20:47:23 -0600

    prettier reducers, formatting only

--- escuela/src/reducers/index.js
+++ escuela/src/reducers/index.js
@@ -1,10 +1,10 @@
-import { combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import usersReducer from './userReducers';
 import userPostReducer from './userPostReducer';
 import flashMessages from './flashMessages';
 
 export default combineReducers({
-    users: usersReducer,
-    posts: userPostReducer,
-    message: flashMessages
+  users: usersReducer,
+  posts: userPostReducer,
+  message: flashMessages
 })
