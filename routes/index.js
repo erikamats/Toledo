e269fa530c40e81cdf660cc1ee231805c98e4517
John <entity.john@gmail.com>
2018-11-23 20:17:29 -0600

    add homepage route so localhost:5000/ can be dev-friendly
    While you are developing on the server, why not just put the function or method you are testing right on the '/' homepage route? That way you just fire up the server, navigate your browser to localhost:5000 and BOOM get an answer from the server.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const gradebookController = require('../controllers/gradebookController');
 const router = express.Router();
 const { catchErrors } = require('../handlers/errorHandlers');
 
+// So if we go to `localhost:5000/` then the following route
+// is what will happen:
+// Good to know for development speed tricks
+router.get('/', gradebookController.getStudents)
+
 // If we were actually using async/await in the controller method we refer to, then we would wrap
 // the method in `catchErrors`
 router.get('/students', gradebookController.getStudents);
