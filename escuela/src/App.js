183e307bf4a23d0a864601e87647bf09c27e6bb0
erikamats <erikamats@hotmail.com>
2018-11-24 21:49:07 -0600

    fix path in addClass located in App.js

--- escuela/src/App.js
+++ escuela/src/App.js
@@ -5,11 +5,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import NavigationBar from './components/nav_msg/NavigationBar';
-import ErrorPage from './components/pages/error/ErrorPage';
-import Gradebook from './components/pages/gradebook/Gradebook';
-import SignupPage from './components/pages/signup/SignupPage';
-import AddClassPage from './components/pages/addClass/AddClassPage';
-import UsersList from './components/pages/users/UsersList';
+import ErrorPage from './components/Pages/error/ErrorPage';
+import Gradebook from './components/Pages/gradebook/Gradebook';
+import SignupPage from './components/Pages/signup/SignupPage';
+import AddClassPage from './components/Pages/addClass/AddClassPage';
+import UsersList from './components/Pages/users/UsersList';
 import MessagesList from './components/nav_msg/MessagesList';
 
 import reducers from './reducers';
