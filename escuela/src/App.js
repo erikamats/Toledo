676406d6d99202c7f4c633068ccdaa758e04357a
John <entity.john@gmail.com>
2018-11-24 20:10:07 -0600

    remove and reorganize escuela/app.js

--- escuela/src/App.js
+++ escuela/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import NavigationBar from './components/NavigationBar';
-import ErrorPage from './components/Pages/ErrorPage';
-import Gradebook from './components/Pages/Gradebook';
-import SignupPage from './components/Pages/SignupPage';
-import AddClassPage from './components/Pages/AddClassPage';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import reducers from './reducers';
-import UsersList from './components/UsersList';
 
-import MessagesList from './components/MessagesList';
+import NavigationBar from './components/nav_msg/NavigationBar';
+import ErrorPage from './components/pages/error/ErrorPage';
+import Gradebook from './components/pages/gradebook/Gradebook';
+import SignupPage from './components/pages/signup/SignupPage';
+import AddClassPage from './components/pages/addClass/AddClassPage';
+import UsersList from './components/pages/users/UsersList';
+import MessagesList from './components/nav_msg/MessagesList';
+
+import reducers from './reducers';
 
 const store = createStore(
   reducers,
@@ -28,22 +28,16 @@ class App extends Component {
     return (
       <div className="App">
         <Provider store={store}>
+          <NavigationBar />
+          <MessagesList />
           <Router>
-          <>
-            <NavigationBar />
-            <MessagesList />
-            <TransitionGroup>
-              <CSSTransition key={location.key} timeout={100} classNames="fade">
-                <Switch location={location}>
-                  <Route exact path="/" component={Gradebook} />
-                  <Route exact path="/users" component={UsersList} />
-                  <Route exact path="/signup" component={SignupPage} />
-                  <Route component={ErrorPage} />
-                  <Route exact path="/addclass" component={AddClassPage} />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          </>
+            <Switch>
+              <Route exact path="/" component={Gradebook} />
+              <Route exact path="/users" component={UsersList} />
+              <Route exact path="/signup" component={SignupPage} />
+              <Route component={ErrorPage} />
+              <Route exact path="/addclass" component={AddClassPage} />
+            </Switch>
           </Router>
         </Provider>
       </div>
