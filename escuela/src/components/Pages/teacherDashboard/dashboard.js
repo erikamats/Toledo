25c84c05cd03643239c5a0ed5f027ac273a01e43
erikamats <erikamats@hotmail.com>
2018-11-26 11:40:30 -0600

    updated days of week header

--- escuela/src/components/Pages/teacherDashboard/dashboard.js
+++ escuela/src/components/Pages/teacherDashboard/dashboard.js
@@ -11,91 +11,111 @@ class Dashboard extends Component {
           <div className="col-10 ">
             <h3> To Complete for the Day:</h3>
             <div id="accordion">
-  <div className="card">
-    <div className="card-header" id="headingOne">
-      <h5 className="mb-0">
-        <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Attendance
-        </button>
-      </h5>
-    </div>
+              <div className="card">
+                <div className="card-header" id="headingOne">
+                  <h5 className="mb-0">
+                    <button
+                      className="btn btn-link"
+                      data-toggle="collapse"
+                      data-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      Attendance
+                    </button>
+                  </h5>
+                </div>
 
-    <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-      <div className="card-body">
-       Import attendance Table here
-       <table className="table table-hover table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-
-      </div>
-    </div>
-  </div>
-  <div className="card">
-    <div className="card-header" id="headingTwo">
-      <h5 className="mb-0">
-        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-         GradeBook
-        </button>
-      </h5>
-    </div>
-    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-      <div className="card-body">
-
-      <GradeBook/>
-
-      Features to Include:
-    
-        Add Assigment
-        Edit/Delete Assingment
-        Add Grade
-        Edit/Delete Assignment
-      </div>
-    </div>
-  </div>
-  <div className="card">
-    <div className="card-header" id="headingThree">
-      <h5 className="mb-0">
-        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Add New Students
-        </button>
-      </h5>
-    </div>
-    <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-      <div className="card-body">
-       <SignupPage/>
-      </div>
-    </div>
-  </div>
-</div>
-
+                <div
+                  id="collapseOne"
+                  className="collapse show"
+                  aria-labelledby="headingOne"
+                  data-parent="#accordion"
+                >
+                  <div className="card-body">
+                    Import attendance Table here
+                    <table className="table table-hover table-dark">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">First</th>
+                          <th scope="col">Last</th>
+                          <th scope="col">Handle</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                      
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-header" id="headingTwo">
+                  <h5 className="mb-0">
+                    <button
+                      className="btn btn-link collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="collapseTwo"
+                    >
+                      GradeBook
+                    </button>
+                  </h5>
+                </div>
+                <div
+                  id="collapseTwo"
+                  className="collapse"
+                  aria-labelledby="headingTwo"
+                  data-parent="#accordion"
+                >
+                  <div className="card-body">
+                    <GradeBook />
+                    Features to Include: Add Assigment Edit/Delete Assingment
+                    Add Grade Edit/Delete Assignment
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-header" id="headingThree">
+                  <h5 className="mb-0">
+                    <button
+                      className="btn btn-link collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseThree"
+                      aria-expanded="false"
+                      aria-controls="collapseThree"
+                    >
+                      Add New Students
+                    </button>
+                  </h5>
+                </div>
+                <div
+                  id="collapseThree"
+                  className="collapse"
+                  aria-labelledby="headingThree"
+                  data-parent="#accordion"
+                >
+                  <div className="card-body">
+                    <SignupPage />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        
         </div>
       </div>
     );
