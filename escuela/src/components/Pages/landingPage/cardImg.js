9d254645ea5a2540e4ade7212d42c912de7c9f84
erikamats <erikamats@hotmail.com>
2018-11-26 15:36:49 -0600

    create IMG component

---
+++ escuela/src/components/Pages/landingPage/cardImg.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const cardImg = (src, imgAlt, imgClassname) => {
+  return <img src={src} alt="imgAlt" className={imgClassname} />;
+};
+
+export default cardImg;
