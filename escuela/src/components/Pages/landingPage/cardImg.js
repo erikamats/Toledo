965fa63aaa242c2339d0f74c134e711028df446e
John <entity.john@gmail.com>
2018-11-27 13:36:52 -0600

    fix component `CardImg` file name
    React Components and their files shall be **capitalized** ;) <3

--- escuela/src/components/Pages/landingPage/cardImg.js
+++ escuela/src/components/Pages/landingPage/cardImg.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const CardImg = ({src, alt, className}) => {
+export const CardImg = ({ src, alt, className }) => {
   return <img src={src} alt={alt} className={className} />
 };
 
