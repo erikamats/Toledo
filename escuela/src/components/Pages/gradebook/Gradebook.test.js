4c8f2a315facb4f30a095d75c8888cad5c60ccb3
John <entity.john@gmail.com>
2018-11-27 05:50:33 -0600

    unable to test redux integration at this time, remove test files

--- escuela/src/components/Pages/gradebook/Gradebook.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Gradebook from './Gradebook';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Gradebook />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
