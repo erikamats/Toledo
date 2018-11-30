66e1ccc1d723eee1ff72b1357df998d355d2ae44
John <entity.john@gmail.com>
2018-11-30 13:54:04 -0600

    add try catch to and clean up to mongoose connection

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 // import environmental variables from our variables.env file
-// require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: 'variables.env' });
 
 // Connect to the Mongo DB
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/27017/Student', { useNewUrlParser: true });
-mongoose.connection.once('open', () => console.log('Connection to MongoDB database was successful'));
+const connectURI = process.env.MONGODB_URI;
+try {
+  console.log('Attempting mongoDB connection')
+  mongoose.connect(connectURI, { useNewUrlParser: true });
+} catch{
+  console.log(`Connection failed. Attempted URI: ${connectURI}`)
+} finally {
+  console.log('Onwards!')
+}
+mongoose.connection.once('open', () => console.log('Connection to MongoDB database was successful!'));
 
 // import all of our models
 require('./models/Student');
