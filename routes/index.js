51cc609c7e10479db85034704aa3c081f3b0bee4
John <entity.john@gmail.com>
2018-11-30 19:42:46 -0600

    update server to respond with sampleData for gradebooks and assignments

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,22 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 const router = express.Router();
 
-// So if we go to `localhost:5000/` then the following route
-// is what will happen:
 router.get('/', homepageController.sendSampleDataAsJSON)
 
-// If we were actually using async/await in the controller method we refer to, then we would wrap
-// the method in `catchErrors`
 router.get('/students', gradebookController.getStudents);
+/*
+If we were actually using async/await 
+in the controller method we refer to, then we would wrap the method in 
+`catchErrors`.
+*/
 router.get('/students-asynchronously', catchErrors(gradebookController.getStudentsAsynchronously));
 router.post('/register-student', gradebookController.addStudent);
 
 router.get('/courses', gradebookController.getCourses);
-router.get('/courses-asynchronously', catchErrors(gradebookController.getCoursesAsynchronously));
 router.post('/register-course', gradebookController.addCourse);
 
+router.get('/gradebooks', gradebookController.getGradebooks);
+
+router.get('/assignments', gradebookController.getAssignments);
 
 module.exports = router;
