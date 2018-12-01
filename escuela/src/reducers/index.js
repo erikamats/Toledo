455815eed16ad6e24472d192771ee17765e5a60b
John <entity.john@gmail.com>
2018-11-30 20:43:51 -0600

    add gradebooksReducer to root reducer

--- escuela/src/reducers/index.js
+++ escuela/src/reducers/index.js
@@ -4,11 +4,13 @@ import userPostReducer from './userPostReducer';
 import flashMessages from './flashMessages';
 import classesReducer from './classesReducer';
 import addCourseReducer from './addCourseReducer';
+import gradebooksReducer from './gradebooksReducer';
 
 export default combineReducers({
-    users: usersReducer,
-    posts: userPostReducer,
-    message: flashMessages,
-    classes:classesReducer,
-    addCourse: addCourseReducer
+  users: usersReducer,
+  posts: userPostReducer,
+  message: flashMessages,
+  classes: classesReducer,
+  addCourse: addCourseReducer,
+  allGradebooks: gradebooksReducer
 })
