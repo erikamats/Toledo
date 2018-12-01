c7386bb0f7512f77782a4741d2151f7bbc6a1cc9
erikamats <erikamats@hotmail.com>
2018-11-30 19:24:09 -0600

    changing route to UserReducer actions

--- escuela/src/reducers/userReducers.js
+++ escuela/src/reducers/userReducers.js
@@ -3,7 +3,7 @@ import { FETCH_USERS } from '../actions/types';
 export default (state = [], action) => {
   switch (action.type) {
     case FETCH_USERS:
-      return action.payload.data;
+      return action.payload.data.sampleStudents;
     default:
       return state;
   }
