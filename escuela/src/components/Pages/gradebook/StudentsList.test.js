b86fd6ab97e4d7f2d3ad336f57608d590d2d08c3
John <entity.john@gmail.com>
2018-12-06 16:14:43 -0600

    fix imports

--- escuela/src/components/Pages/gradebook/StudentsList.test.js
+++ escuela/src/components/Pages/gradebook/StudentsList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StudentsList from './StudentsList';
+import StudentsList from './components/StudentsList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
