0d616a05f180245c206ebf4d120ca23b8c873398
John <entity.john@gmail.com>
2018-12-06 16:15:47 -0600

    add/fix key props
    causing errors

--- escuela/src/components/Pages/gradebook/components/GradeTable.js
+++ escuela/src/components/Pages/gradebook/components/GradeTable.js
@@ -4,28 +4,28 @@ import GradeTableHeader from "./GradeTableHeader";
 import GradeItem from "./GradeItem";
 
 
- const GradeTable = (props) => {
+const GradeTable = (props) => {
 
-//   render(props) {
-    const { students } = props;
-    return (
-      <Table hover>
-        <GradeTableHeader />
-    
-        <tbody>
-        {students && students.map(student =>(
-            <GradeItem students={student.firstName} />
+  //   render(props) {
+  const { students } = props;
+  return (
+    <Table hover>
+      <GradeTableHeader />
+
+      <tbody>
+        {students && students.map(student => (
+          <GradeItem key={student.id} students={student.firstName} />
         ))}
-          {/* <GradeItem studentName="Erika" />
+        {/* <GradeItem studentName="Erika" />
           <GradeItem studentName="Bob" />
           <GradeItem studentName="John" />
           <GradeItem studentName="Fernando" /> */}
-        </tbody>
-      </Table>
-      //close return
-    );
-    //close render
-//   }
+      </tbody>
+    </Table>
+    //close return
+  );
+  //close render
+  //   }
   //close component
 }
 export default GradeTable;
