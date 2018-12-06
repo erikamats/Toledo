7aaf59e48614156563c07b685b4dd2af0c55837b
John <entity.john@gmail.com>
2018-12-06 17:09:45 -0600

    fix imports

--- escuela/src/components/footer/Footer.js
+++ escuela/src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Button, Form, Input } from "reactstrap";
-// import FooterWidget from "./footerwidget";
-import FooterListItem from "./footerListitem";
+// import FooterWidget from "./FooterWidget";
+import FooterListItem from "./FooterListitem";
 import moment from "moment";
 
 let now = moment().format("YYYY");
