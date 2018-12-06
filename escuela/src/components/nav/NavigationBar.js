c6d0f959a735948d931ae6211347ab56fe5d6aff
John <entity.john@gmail.com>
2018-12-06 17:10:14 -0600

    remove duplicate nav item

--- escuela/src/components/nav/NavigationBar.js
+++ escuela/src/components/nav/NavigationBar.js
@@ -12,7 +12,8 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem } from 'reactstrap';
+  DropdownItem
+} from 'reactstrap';
 
 
 export default class Example extends React.Component {
@@ -40,27 +41,22 @@ export default class Example extends React.Component {
               <NavItem>
                 <NavLink to="/" className="nav-link" >
                   Home
-                </NavLink>              
+                </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink to="/signup" className="nav-link" >
                   Register
-                </NavLink>              
-              </NavItem>
-              <NavItem>
-                <NavLink to="/signup" className="nav-link" >
-                  Register
-                </NavLink>              
+                </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink to="/gradebook" className="nav-link" >
                   Gradebook
-                </NavLink>              
+                </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink to="/users" className="nav-link" >
                   DB Users
-                </NavLink>              
+                </NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
@@ -68,10 +64,10 @@ export default class Example extends React.Component {
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem href="/courses">
-                      View Courses
+                    View Courses
                   </DropdownItem>
                   <DropdownItem href="/addCourses">
-                      Add Courses
+                    Add Courses
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
