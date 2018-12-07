673839190da0222a244ef71f8485ee6b0b585487
John <entity.john@gmail.com>
2018-12-06 23:12:33 -0600

    get your redux outta my App.js !

--- escuela/src/App.js
+++ escuela/src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import './App.css';
 
 import NavigationBar from './components/nav/NavigationBar';
 import MessagesList from './components/flashMessages/MessagesList';
@@ -12,43 +8,31 @@ import LandingPage from './components/landingPage/LandingPage';
 import ErrorPage from './components/error/ErrorPage';
 import Gradebook from './components/gradebook/Gradebook';
 import SignupPage from './components/signup/SignupPage';
-// import AddClassPage from './components/addClass/AddClassPage';
 import UsersList from './components/users/UsersList';
 import Assignment from "./components/assignments/Assignment";
 import Footer from "./components/footer/Footer";
 
-import reducers from './reducers';
-
-const store = createStore(
-  reducers,
-  {},
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-  )
-);
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <NavigationBar />
-            <MessagesList />
-            <Switch>
-              <Route exact path="/" component={LandingPage} />
-              <Route exact path="/assignment" component={Assignment} />
-              <Route exact path="/gradebook" component={Gradebook} />
-              <Route exact path="/users" component={UsersList} />
-              <Route exact path="/signup" component={SignupPage} />
-              <Route exact path="/dashboard" component={SignupPage} />
-              <Route component={ErrorPage} />
-            </Switch>
-            <Footer />
-          </div>
-        </Router>
-      </Provider>
+      <Router>
+        <div className="App">
+          <NavigationBar />
+          <MessagesList />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/assignment" component={Assignment} />
+            <Route exact path="/gradebook" component={Gradebook} />
+            <Route exact path="/users" component={UsersList} />
+            <Route exact path="/signup" component={SignupPage} />
+            <Route exact path="/dashboard" component={SignupPage} />
+            <Route component={ErrorPage} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
