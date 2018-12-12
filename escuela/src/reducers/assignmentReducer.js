6f4b8530086586e89674f6963e049e0908797592
John <entity.john@gmail.com>
2018-12-11 18:51:45 -0600

    update reducers

--- escuela/src/reducers/assignmentReducer.js
+++ escuela/src/reducers/assignmentReducer.js
@@ -3,9 +3,21 @@ import { updateObject } from './utilityFunctions'
 
 export default (state = {}, action) => {
   switch (action.type) {
-    case ActionTypes.ADD_ASSIGNMENT:
+    case ActionTypes.ADD_DB_ASSIGNMENT_STARTED:
       return updateObject(state, {
-        [action.payload.id]: action.payload
+        isLoading: true,
+        error: false
+      })
+    case ActionTypes.ADD_DB_ASSIGNMENT_SUCCESS:
+      return updateObject(state, {
+        [action.payload.id]: action.payload,
+        isLoading: false,
+        error: false
+      })
+    case ActionTypes.ADD_DB_ASSIGNMENT_FAILURE:
+      return updateObject(state, {
+        isLoading: false,
+        error: action.error
       })
     case ActionTypes.REMOVE_ASSIGNMENT:
       return updateObject(state, {
