297b1d4ed9dbb96b0e0989fb1807eed34427fc82
John <entity.john@gmail.com>
2018-12-17 23:52:15 -0600

    streamline data display of StudentsList
    if you want a more complete picture of the data, console.dir the data in the getStudents action

--- escuela/src/components/gradebook/components/StudentsList.js
+++ escuela/src/components/gradebook/components/StudentsList.js
@@ -5,20 +5,13 @@ export default (props) => {
   // const { isLoading, error } = students
   return (
     <div>
-      <h2>Student List</h2>
+      <h2>Students List</h2>
       <ul>
         {students.students && students.students.map((student) => {
-          const { id, firstName, lastName, gender, dateEnrolled, currentGradeLevel } = student
+          const { _id, firstName, lastName, gender, dateEnrolled, currentGradeLevel } = student
           return (
-            <li key={id}>
-              <h5>{firstName}{' '}{lastName}</h5>
-              <p>Gender: {gender}</p>
-              <p>Date Enrolled:
-              {dateEnrolled
-                  ? new Date(dateEnrolled).toDateString()
-                  : ''}
-              </p>
-              <p>Current Grade Level: {currentGradeLevel}</p>
+            <li key={_id}>
+              <p>{`${firstName} ${lastName} `}<b>Grade {currentGradeLevel}</b></p>
             </li>
           )
         })}
