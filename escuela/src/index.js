4176422effce52a10df93385d0776cdf1bfadf68
John <entity.john@gmail.com>
2018-12-20 11:03:08 -0600

    get json web token from localStorage when app launches

--- escuela/src/index.js
+++ escuela/src/index.js
@@ -3,10 +3,24 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './App';
 import configureStore from './store/configureStore'
+
+import jwt_decode from 'jwt-decode';
+import { setAuthToken } from './reducers/utilityFunctions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
 import * as serviceWorker from './serviceWorker';
 
-
 const store = configureStore()
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = '/login'
+  }
+}
 
 const renderApp = () =>
   render(
