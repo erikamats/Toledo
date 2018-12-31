778511f8afb49968ac6665ced9648fc049ee4b0a
erikamats <erikamats@hotmail.com>
2018-12-30 23:16:30 -0600

    more prettifying dashboard

--- escuela/src/components/teacherDashboard/components/DashRow2.js
+++ escuela/src/components/teacherDashboard/components/DashRow2.js
@@ -9,12 +9,12 @@ const DashRow2 = () => {
       <Col lg="3">
         <LandingCard
           colClassName="col-12"
-          divClassName="text-center py-3"
+          divClassName="text-center py-3 text-white"
           style={{
-            backgroundColor: "white",
-            border: "2px solid",
-            borderColor: "#65b12d",
-            borderRadius: "5px",
+            backgroundColor: "teal",
+            // border: "2px solid",
+            // borderColor: "#65b12d",
+            borderRadius: "3px",
             color: "#00000"
           }}
           dashHeaderText="Today's Attendance "
@@ -31,12 +31,12 @@ const DashRow2 = () => {
       <Col lg="3">
         <LandingCard
           colClassName="col-12"
-          divClassName="text-center py-3"
+          divClassName="text-center py-3 text-black"
           style={{
             backgroundColor: "white",
-            border: "2px solid",
-            borderColor: "var(--medNavy)",
-            borderRadius: "5px",
+            // border: "2px solid",
+            // borderColor: "var(--medNavy)",
+            borderRadius: "3px",
             color: "#000000"
           }}
           dashHeaderText="Constellation Homework"
@@ -44,7 +44,7 @@ const DashRow2 = () => {
           // date={moment().format('dddd, MMMM Do')}
           secondHeader="Avg Grade:"
           secondHeaderClassName=" pt-2"
-          subHeaderClassName="text-default"
+          subHeaderClassName="text-default text-warning"
           subHeader="85%"
           href="/gradebook"
           btnText="Edit Grades"
@@ -54,12 +54,12 @@ const DashRow2 = () => {
       <Col lg="3">
         <LandingCard
           colClassName="col-12"
-          divClassName="text-center py-3"
+          divClassName="text-center py-3 text-default"
           style={{
             backgroundColor: "white",
-            border: "2px solid",
-            borderColor: "var(--medNavy)",
-            borderRadius: "5px",
+            // border: "2px solid",
+            // borderColor: "var(--medNavy)",
+            borderRadius: "3px",
             color: "#000000"
           }}
           dashHeaderText="Constellation Quiz"
@@ -67,7 +67,7 @@ const DashRow2 = () => {
           // date={moment().format('dddd, MMMM Do')}
           secondHeader="Avg Grade:"
           secondHeaderClassName=" pt-2"
-          subHeaderClassName="text-default"
+          subHeaderClassName="text-warning"
           subHeader="--"
           href="/gradebook"
           btnText="Edit Grades"
@@ -77,20 +77,20 @@ const DashRow2 = () => {
       <Col lg="3">
         <LandingCard
           colClassName="col-12"
-          divClassName="text-center py-3"
+          divClassName="text-center py-3 text-default"
           style={{
             backgroundColor: "white",
-            border: "2px solid",
-            borderColor: "#eb5151",
-            borderRadius: "5px",
-            color: "#000000"
+            // border: "2px solid",
+            // borderColor: "#eb5151",
+            borderRadius: "3px",
+            color: "black"
           }}
           dashHeaderText="Constellation Exam"
           subText="Due 11/12"
           // date={moment().format('dddd, MMMM Do')}
           secondHeader="Avg Grade:"
           secondHeaderClassName=" pt-2"
-          subHeaderClassName="text-default"
+          subHeaderClassName="text-danger"
           subHeader="--"
           href="/gradebook"
           btnText="Edit Grades"
--- escuela/src/components/teacherDashboard/components/DashRow4.js
+++ escuela/src/components/teacherDashboard/components/DashRow4.js
@@ -1,81 +1,79 @@
 import React from "react";
-import {  Row,
-    Col,
-    Card,
-    Button,
-    CardTitle,
-    CardText,
-    CardHeader,
-    CardBody } from "reactstrap";
+import { Row, Col, Button } from "reactstrap";
 import Calendar, { External } from "./Calendar";
-
 
 const DashRow4 = () => {
   return (
     <Row className="dashRow-4">
-          <Col lg={{ size: "2" }}>
-            <External />
-          </Col>
-          <Col lg={{ size: "4" }}>
-            <Calendar />
-          </Col>
-          <Col lg={{ size: "3" }}>
-            <Card body className="text-center">
-              <CardTitle>Reminders/Notes</CardTitle>
-              <CardText>
-                <ul>
-                  <li>
-                    {" "}
-                    <h4>List item 1</h4>
-                  </li>
-                  <li>
-                    {" "}
-                    <h4>List item 2</h4>
-                  </li>
-                  <li>
-                    {" "}
-                    <h4>List item 3</h4>
-                  </li>
-                  <li>
-                    {" "}
-                    <h4>List item 4</h4>
-                  </li>
-                  <li>
-                    {" "}
-                    <h4>List item 5</h4>
-                  </li>
-                  <li>
-                    {" "}
-                    <h4>List item 6</h4>
-                  </li>
-                </ul>
-              </CardText>
-              <Button>New Reminder/Note</Button>
-            </Card>
-          </Col>
+      {/* <Col lg="6"> */}
+      <Col lg="2">
+        <External />
+      </Col>
+      <Col lg="4">
+        <Calendar />
+      </Col>
+      {/* </Col> */}
+      <Col lg="3">
+        <div className="white-box" style={{ backgroundColor: "lightyellow" }}>
+          <h4 className="box-title">
+            {" "}
+            Notes &nbsp; <i class="fas fa-vote-yea" />
+          </h4>
+          <ul className="basic-list">
+            <li>Note Item</li>
+            <li>
+              A Note
+              <span className="float-right">
+                <i class="fas fa-trash-alt" />
+              </span>
+            </li>
+            <li>
+              Another Note
+              <span className="float-right">
+                <i class="fas fa-trash-alt" />
+              </span>
+            </li>
+            <li>
+              Another Note
+              <span className="float-right">
+                <i class="fas fa-trash-alt" />
+              </span>
+            </li>
+          </ul>
+          <Button className="mt-3 col-10 offset-1"> New Item</Button>
+        </div>
+      </Col>
 
-          <Col lg={{ size: "3" }}>
-            <Card>
-              <CardHeader>
-                <h3>Birthdays</h3>
-              </CardHeader>
-              <CardBody>
-                <CardText>
-                  <Col lg="6" className="float-left">
-                    <p> Today</p>
-                    <p> 11/15</p>
-                    <p> 11/16</p>
-                  </Col>
-                  <Col lg="6" className="float-right">
-                    <p>Erika</p>
-                    <p>Kevin</p>
-                    <p> Maria</p>
-                  </Col>
-                </CardText>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+      <Col lg="3">
+        <div className="white-box">
+          <h4 className="box-title">
+            Birthdays &nbsp;<i class="fas fa-gift" />
+          </h4>
+          <ul className="basic-list">
+            <li>
+              Student Name
+              <span className="float-right label ">Date</span>
+            </li>
+            <li>
+              Erika
+              <span className="float-right label">11/3</span>
+            </li>
+            <li>
+              John
+              <span className="float-right label">11/4</span>
+            </li>
+            <li>
+              Daniel
+              <span className="float-right label">11/5</span>
+            </li>
+            <li>
+              Fernando
+              <span className="float-right label">11/30</span>
+            </li>
+          </ul>
+        </div>
+      </Col>
+    </Row>
   );
 };
 export default DashRow4;
