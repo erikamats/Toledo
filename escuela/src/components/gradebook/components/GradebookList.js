e3bf69f1ff0f918c61b1897251f6309d429645e3
John <entity.john@gmail.com>
2019-01-17 18:03:53 -0600

    Revert "fix GradebookList component"
    This reverts commit 6d917a0f3dbf2b371318bba45f754001e71b1343.
    
    I overwrote this file, but still needed some pieces from it, so I reverted it, knowing that I already had a new component with code so nothing was lost

--- escuela/src/components/gradebook/components/GradebookList.js
+++ escuela/src/components/gradebook/components/GradebookList.js
@@ -1,21 +1,28 @@
 import React from 'react'
-import { Button } from 'reactstrap';
 
 export default (props) => {
-  const { gradebooks } = props;
+  const { gradebooks, assignments } = props;
   return (
     <div>
-      {gradebooks.gradebooks && gradebooks.gradebooks.map((gradebook) => {
-        const { gradebookName, courseCommonName, gradePortions, associatedCourseId, assignmentsIdArray } = gradebook;
-        return (
-          <Button
-            key={gradebookName}
-            color="primary"
-          >
-            {gradebookName}
-          </Button>
-        )
-      })}
+      <ul>
+        {gradebooks.gradebooks && gradebooks.gradebooks.map((gradebook) => {
+          const { gradebookName, gradePortions, associatedCourseId, assignmentsInGradebook } = gradebook;
+          return (
+            <li key={gradebookName}>
+              <h3>{gradebookName}</h3>
+              <p>associatedCourseId: {associatedCourseId}</p>
+              <h5>gradePortions</h5>
+              <ul>{gradePortions.map(portion => (
+                <li key={portion.portionName}>
+                  <h6>{portion.portionName}</h6>
+                  <p>{portion.gradeWeight0to1}</p>
+                </li>
+              ))}
+              </ul>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
