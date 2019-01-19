a1bbecf2ac879a9151c1d314b82f32a8bc0787a5
John <entity.john@gmail.com>
2019-01-18 23:24:52 -0600

    add react state for conditional render
    We need to keep track of whether we are looking at our gradebook overview, or if we are looking at the edit form for a selected gradebook. I chose to keep track of this UI-related issue in React, no Redux action needed.

--- escuela/src/components/gradebook/Gradebook.js
+++ escuela/src/components/gradebook/Gradebook.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 // import GradeTable from "./components/GradeTable";
 import GradebookButtonsList from "./components/GradebookButtonsList";
+import OpenGradebook from "./components/OpenGradebook";
 // import StudentsList from "./components/StudentsList";
 // import CourseList from "./components/CourseList";
 
 
 export default class Gradebook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditingGradebook: false
+    };
+    this.toggleViewEditGradebook.bind(this);
+  }
   componentWillMount = () => {
     const { getAssignments, getStudents, getGradebooks, getCourses } = this.props;
     getGradebooks();
@@ -13,13 +21,20 @@ export default class Gradebook extends Component {
     getStudents();
     getCourses();
   }
+  toggleViewEditGradebook = () => {
+    this.setState({
+      isEditingGradebook: !this.state.isEditingGradebook
+    })
+  }
 
   render() {
-    const { gradebooks, students, assignments, selectGradebookForEditing } = this.props
+    const { gradebooks, students, assignments, selectGradebookForEditing, currentlyEditingGradebook } = this.props
+    if (this.state.isEditingGradebook) { return <OpenGradebook gradebook={currentlyEditingGradebook} toggleViewEditGradebook={this.toggleViewEditGradebook} /> }
     return (
       <div>
+
         <h2>Your Gradebooks</h2>
-        <GradebookButtonsList selectGradebookForEditing={selectGradebookForEditing} gradebooks={gradebooks} />
+        <GradebookButtonsList toggleViewEditGradebook={this.toggleViewEditGradebook} selectGradebookForEditing={selectGradebookForEditing} gradebooks={gradebooks} />
       </div>
     )
   }
